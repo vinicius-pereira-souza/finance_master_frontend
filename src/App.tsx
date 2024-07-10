a09df7b7975af9210bd9b2c0bc9e370a38c3e1d3
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2024-07-09 21:30:52 -0300

    chore: configuração inicial do projeto completo

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <p>Inicialização de um novo projeto</p>
+    </main>
+  );
+}
+
+export default App;
